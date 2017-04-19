c4979dcef336a0f893ea0d50bb4ab218457f9905
Kevin Schoon <kevinschoon@gmail.com>
2017-04-19 19:40:43 -0400

    add discover-ip script

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,22 @@ PIDS=""
   PIDS="$PIDS $!"
 }
 
+# Libprocess MUST be able to resolve our hostname
+# Some environments such as runc don't automatically
+# specify this like Docker does. It cna also be used
+# with the --discover-ip flag
+cat > /sbin/discover-ip <<-__EOF__ 
+#!/bin/sh
+ip addr |grep 'state UP' -A2 |tail -n1 | awk '{print \$2}' | sed 's/\/.*//'
+__EOF__
+
+chmod +x /sbin/discover-ip
+
+[ -n "$DISCOVER_IP" ] && {
+  sleep 10
+  echo "$(discover-ip)      $(hostname)" >> /etc/hosts
+}
+
 # Wait for all processes to complete.
 if [ "$PIDS" != "" ]; then
   wait $PIDS
